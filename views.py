e9f4d410adf05191930eaee0fb67d63f1c4d1603
user <user@DESKTOP-9LNGS3L>
2025-03-27 16:33:35 -0700

    Add project files.

---
+++ views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from .forms import ObituaryForm
+
+def submit_obituary(request):
+    if request.method == "POST":
+        form = ObituaryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('view_obituaries')
+    else:
+        form = ObituaryForm()
+    return render(request, 'obituary_form.html', {'form': form})
